48666cc194db702ee86c3177ff1a9911445b1d16
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2019-08-13 15:06:35 +0200

    Directly hook into pipeline

--- frameworks/FSharp/aspnetcore-fsharp/PlatformBenchmarks/Program.fs
+++ frameworks/FSharp/aspnetcore-fsharp/PlatformBenchmarks/Program.fs
@@ -3,13 +3,43 @@ module App.App
 
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Server.Kestrel.Core
+open Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
 open System
+open Microsoft.AspNetCore.Connections
 open System.Text
 open System.Net
 open System.IO.Pipelines
 open System.Buffers
+open System.Threading.Tasks
+open FSharp.Control.Tasks
+open Microsoft.AspNetCore.Builder
 
 let ApplicationName = "Kestrel Platform-Level Application in F#"
+
+module Connection =
+
+    type IHttpConnection =
+        inherit IHttpHeadersHandler
+        inherit IHttpRequestLineHandler
+
+        abstract member Reader : PipeReader with get, set
+        abstract member Writer : PipeWriter with get, set
+
+        abstract member ExecuteAsync : unit -> Task
+
+
+    type HttpApplication<'TConnection when 'TConnection :> IHttpConnection and 'TConnection : (new : unit -> 'TConnection)>() =
+        member __.ExecuteAsync(connection:ConnectionContext) =
+            let httpConnection = new 'TConnection()
+            httpConnection.Reader <- connection.Transport.Input
+            httpConnection.Writer <- connection.Transport.Output
+            httpConnection.ExecuteAsync()
+
+    let UseHttpApplication<'TConnection when 'TConnection :> IHttpConnection and 'TConnection : (new : unit -> 'TConnection)> (builder:IConnectionBuilder) =
+        builder.Use(fun _next ->
+            let httpApplication = new HttpApplication<'TConnection>()
+            new ConnectionDelegate(httpApplication.ExecuteAsync))
+
 
 module Paths =
     let Plaintext = "/plaintext"
@@ -30,12 +60,15 @@ module Endpoints =
     let createPlaintext() =
         IPEndPoint(IPAddress.Loopback, 8080)
 
-module Writers =
+module Application =
+    type Startup() =
+        member this.Configure(app:IApplicationBuilder) =
+            ()
+
     type WriterAdapter(writer:PipeWriter) =
         struct
         end
             interface IBufferWriter<byte> with
-
                 member __.Advance(count:int) = writer.Advance(count)
                 member __.GetMemory(sizeHint:int) = writer.GetMemory(sizeHint)
                 member __.GetSpan(sizeHint:int) = writer.GetSpan(sizeHint)
@@ -67,6 +100,33 @@ module Writers =
         writer.Write(ReadOnlySpan<byte> Texts._plainTextBody)
         // TODO: writer.Commit()
 
+    type MyApp() =
+        member val Reader = Unchecked.defaultof<PipeReader> with get, set
+        member val Writer = Unchecked.defaultof<PipeWriter> with get, set
+
+
+        interface Connection.IHttpConnection with
+            member self.Reader with get () = self.Reader and set v = self.Reader <- v
+            member self.Writer with get () = self.Writer and set v = self.Writer <- v
+
+
+            override __.OnStartLine(method:HttpMethod, version:Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http.HttpVersion, target:Span<byte>, path: Span<byte>, query: Span<byte>, customMethod:Span<byte>, pathEncoded:bool) =
+                ()
+
+            override __.OnHeader(name:Span<byte>, value: Span<byte>) =
+                ()
+
+            override __.OnHeadersComplete() =
+                ()
+
+            member self.ExecuteAsync() =
+                let t = task {
+                    plainText self.Writer
+                    self.Writer.Complete()
+
+                }
+                t :> Task
+
 
 [<EntryPoint>]
 let main args =
@@ -77,11 +137,19 @@ let main args =
     let host =
         WebHostBuilder()
             // TODO: .UseBenchmarksConfiguration(config)
-            .UseKestrel()
+            .UseKestrel(fun (options:KestrelServerOptions) ->
+                let endPoint = Endpoints.createPlaintext()
+
+                options.Listen(endPoint, (fun (listenOptions:ListenOptions) ->
+                    let builder = listenOptions :> IConnectionBuilder
+                    Connection.UseHttpApplication<Application.MyApp> builder |> ignore
+                ))
+            )
+            .UseStartup<Application.Startup>()
             .Build()
 
 
-    host.Run(fun (ctx:HttpContext) -> ctx.Response.BodyPipe <- plainText)  // ???
+    host.Run()
     0
 
 
